540d7f9e359dabf51f8b4ef519f61d1363619b94
lethingoctham <72124921+lethingoctham@users.noreply.github.com>
2020-11-23 16:01:16 +0700

    Add files via upload

---
+++ bt 5.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 23 15:08:07 2020
+
+@author: acer
+"""
+
+import math
+list1= [136,1425,542,15,2020,2000,9999]
+i=0
+for i in range(len(list1)):
+    print(list1[i])
+print("ket thuc chuong trinh 1")
+i=0
+for i in range(len(list1)):
+    print(list1[i])
+    print("logarit la:",math.log10(list1[i]))
+print("ket thuc chuong trinh 2")
